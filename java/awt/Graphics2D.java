0d82365c92b3569f1811e152f886a0d51d1c8deb
Allan (Slam) <72238104+aayes89@users.noreply.github.com>
2026-09-02 08:00:09 -0600

    Extend Graphics2D class with additional drawing methods

--- java/awt/Graphics2D.java
+++ java/awt/Graphics2D.java
@@ -24,9 +24,14 @@ package java.awt;
 
 import kernel.Native;
 
-public class Graphics2D {
+public class Graphics2D extends Graphics{
     private Color currentColor = Color.BLACK;
 
+    public Graphics2D(){
+        super();
+    }
+
+    @Override
     public void setColor(Color c) {
         if (c != null) {
             this.currentColor = c;
@@ -34,39 +39,39 @@ public class Graphics2D {
             Native.sys(Native.SYS_SET_COLOR, c.getRGB(), 0, 0, 0);
         }
     }
-
+    @Override
     public Color getColor() {
         return currentColor;
     }
-
+    @Override
     public void fillRect(int x, int y, int width, int height) {
 		//Syscall 2: Rellenar rectángulo
         Native.sys(Native.SYS_FILL_RECT, x, y, width, height);
     }
-
+    @Override
     public void drawRect(int x, int y, int width, int height) {
 		// Syscall 3: Dibujar borde rectángulo
         Native.sys(Native.SYS_DRAW_RECT, x, y, width, height);
     }
-
+    @Override
     public void drawLine(int x1, int y1, int x2, int y2) {
 		// Syscall 4: Dibujar línea
         Native.sys(Native.SYS_DRAW_LINE, x1, y1, x2, y2);
     }
-
+    @Override
     public void drawString(String text, int x, int y) {
         if (text != null) {
 			// Syscall 5: Imprimir cadena texto en modo gráfico
             Native.sys(Native.SYS_DRAW_STRING, x, y, text, 0);
         }
     }    
-	
+	@Override
 	public int drawChar(char c, int x, int y) {
         // Syscall 15: Renderizar carácter en VRAM
         Native.sys(Native.SYS_DRAW_CHAR, x, y, (int) c, 0);
         return x + 10; // Devuelve la siguiente posición X
     }
-	
+	@Override
 	public int drawInt(int value, int x, int y) {
         if (value == 0) {
             drawChar('0', x, y);
@@ -106,7 +111,7 @@ public class Graphics2D {
         
         return endX;
     }
-    
+    @Override
     public int getPixel(int x, int y) {
 		// Syscall 14: Leer pixel de VRAM
         return Native.sys(Native.SYS_GET_PIXEL, x, y, 0, 0);
