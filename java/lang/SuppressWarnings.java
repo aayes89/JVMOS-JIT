fbbd59648ff8738869503c1f5e08c04a6ef5fc06
Allan (Slam) <72238104+aayes89@users.noreply.github.com>
2026-09-01 23:07:35 -0600

    Reformat Target annotation for better readability

--- java/lang/SuppressWarnings.java
+++ java/lang/SuppressWarnings.java
@@ -27,10 +27,15 @@ import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 
-@Target( { ElementType.TYPE, ElementType.FIELD, ElementType.METHOD,
-        ElementType.PARAMETER, ElementType.CONSTRUCTOR,
-        ElementType.LOCAL_VARIABLE })
+@Target({
+    ElementType.TYPE, 
+    ElementType.FIELD, 
+    ElementType.METHOD,
+    ElementType.PARAMETER, 
+    ElementType.CONSTRUCTOR,
+    ElementType.LOCAL_VARIABLE
+})
 @Retention(RetentionPolicy.SOURCE)
 public @interface SuppressWarnings {
-    public String[] value();
+    String[] value();
 }
