ed10718d6034f0ad60f7729a6c58e1a268d67376
Allan (Slam) <72238104+aayes89@users.noreply.github.com>
2026-09-02 00:45:04 -0600

    Enhance PrintStream with Spanish comments and methods
    Added Spanish comments for clarity and improved method definitions for printing various data types.

--- java/io/PrintStream.java
+++ java/io/PrintStream.java
@@ -26,11 +26,14 @@ import kernel.Native;
 
 public class PrintStream {
     
-    // constructor
+    // Constructor por defecto requerido para instanciar System.out
     public PrintStream() {}
 
+    // Cadena de caracteres
     public void print(String s) {
-        if (s == null) s = "null";
+        if (s == null) {
+            s = "null";
+        }
         Native.sys(Native.SYS_SERIAL_PUTS, 0, 0, s, 0); 
     }
     
@@ -39,12 +42,13 @@ public class PrintStream {
         println();
     }
 
-    // Sobrecarga exigida cuando se hace System.out.println(); vacío
+    // Salto de línea sin parámetros ()V
     public void println() {
         Native.sys(Native.SYS_SERIAL_PUTC, '\r', 0, 0, 0);
         Native.sys(Native.SYS_SERIAL_PUTC, '\n', 0, 0, 0);
     }
     
+    // Enteros
     public void print(int value) {
         if (value == 0) {
             Native.sys(Native.SYS_SERIAL_PUTC, '0', 0, 0, 0);
@@ -53,9 +57,17 @@ public class PrintStream {
         int temp = value;
         int len = 0;
         boolean isNeg = false;
-        if (temp < 0) { isNeg = true; temp = -temp; value = temp; len++; }
+        if (temp < 0) { 
+            isNeg = true; 
+            temp = -temp; 
+            value = temp; 
+            len++; 
+        }
         int t2 = temp;
-        while (t2 > 0) { len++; t2 /= 10; }
+        while (t2 > 0) { 
+            len++; 
+            t2 /= 10; 
+        }
         
         int currX = len;
         byte[] chars = new byte[len];
@@ -64,9 +76,11 @@ public class PrintStream {
             chars[--currX] = (byte)('0' + (temp % 10));
             temp /= 10;
         }
-        if (isNeg) chars[0] = '-';
+        if (isNeg) {
+            chars[0] = '-';
+        }
         
-        for(int i = 0; i < len; i++) {
+        for (int i = 0; i < len; i++) {
             Native.sys(Native.SYS_SERIAL_PUTC, chars[i], 0, 0, 0);
         }
     }
@@ -76,28 +90,62 @@ public class PrintStream {
         println();
     }
 
-    // SOBRECARGAS OBLIGATORIAS PARA EL JIT
-    // Redirijo la impresión a los métodos nativos para evitar Kernel Panics
+    // Caracteres
+    public void print(char c) { 
+        Native.sys(Native.SYS_SERIAL_PUTC, c, 0, 0, 0); 
+    }
     
-    public void print(Object obj) { print(String.valueOf(obj)); }
-    public void println(Object obj) { println(String.valueOf(obj)); }
-
-    public void print(long l) { print(String.valueOf(l)); }
-    public void println(long l) { println(String.valueOf(l)); }
-
-    public void print(float f) { print(String.valueOf(f)); }
-    public void println(float f) { println(String.valueOf(f)); }
-
-    public void print(double d) { print(String.valueOf(d)); }
-    public void println(double d) { println(String.valueOf(d)); }
-
-    public void print(boolean b) { print(String.valueOf(b)); }
-    public void println(boolean b) { println(String.valueOf(b)); }
-
-    // Optimizado para usar el syscall SYS_SERIAL_PUTC
-    public void print(char c) { Native.sys(Native.SYS_SERIAL_PUTC, c, 0, 0, 0); }
     public void println(char c) { 
         print(c); 
+        println(); 
+    }
+
+    // Flotantes y Dobles (Formateo mediante String.valueOf para evitar cuelgues FPU)
+    public void print(float f) { 
+        print(String.valueOf(f)); 
+    }
+    
+    public void println(float f) { 
+        print(f); 
+        println(); 
+    }
+
+    public void print(double d) { 
+        print(String.valueOf(d)); 
+    }
+    
+    public void println(double d) { 
+        print(d); 
+        println(); 
+    }
+
+    // Enteros Largos (long)
+    public void print(long l) { 
+        print(String.valueOf(l)); 
+    }
+    
+    public void println(long l) { 
+        print(l); 
+        println(); 
+    }
+
+    // Booleanos
+    public void print(boolean b) { 
+        print(String.valueOf(b)); 
+    }
+    
+    public void println(boolean b) { 
+        print(b); 
+        println(); 
+    }
+
+    // Objetos genéricos
+    public void print(Object obj) { 
+        print(String.valueOf(obj)); 
+    }
+    
+    public void println(Object obj) { 
+        print(obj); 
         println(); 
     }
 }
