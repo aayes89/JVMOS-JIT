64764800d4cc03efbf2382cb1e6a5d354a6baefa
Allan (Slam) <72238104+aayes89@users.noreply.github.com>
2026-09-04 11:59:37 -0600

    Refactor DiskIO constructor and methods for clarity

--- java/io/DiskIO.java
+++ java/io/DiskIO.java
@@ -41,74 +41,78 @@ public class DiskIO {
     }
 
     // Constructor para múltiples discos
-    public DiskIO(int deviceId) {
-        this.deviceId = deviceId;
+    public DiskIO(int id) {
+        deviceId = id;
         // Syscall 0: SYS_KALLOC
 		// Asignar 512 bytes exactos en el Heap para operaciones de sector
-        this.bufferAddr = Native.sys(Native.SYS_KALLOC, 512, 0, 0, 0);
+        bufferAddr = Native.sys(Native.SYS_KALLOC, 512, 0, 0, 0);
     }
 
     // Lee un sector crudo a un arreglo de bytes (para sistemas de archivos)
     public byte[] readSector(int lba) {
         // Syscall 8: SYS_DISK_READ (Leer sector ATA)
-        Native.sys(Native.SYS_DISK_READ, lba, 0, this.bufferAddr, 0);
-        
-        byte[] data = new byte[512];
-        for (int i = 0; i < 512; i++) {
-			// Syscall 27: SYS_MEM_READ_BYTE
-            data[i] = (byte) Native.sys(27, this.bufferAddr + i, 0, 0, 0); 
-        }
-        return data;
+        int resultRead = Native.sys(Native.SYS_DISK_READ, lba, 0, bufferAddr, 0);
+        if(resultRead == 1){
+			byte[] data = new byte[512];
+			for (int i = 0; i < 512; i++) {
+				// Syscall 27: SYS_MEM_READ_BYTE
+				data[i] = (byte) Native.sys(27, bufferAddr + i, 0, 0, 0); 
+			}
+			return data;
+		}
+		// devolver arreglo vacio, evitar NULL
+		return new byte[0];
     }
 
     // Lee un sector y lo interpreta directamente como String (hasta encontrar un byte 0)    
 	public String readString(int lba) {
 		// Syscall 8: SYS_DISK_READ (Leer sector ATA)
-        Native.sys(Native.SYS_DISK_READ, lba, 0, this.bufferAddr, 0);
-        
-        byte[] bytes = new byte[512];
-        int length = 0;
-        
-        for (int i = 0; i < 512; i++) {
-			// Syscall 27: SYS_MEM_READ_BYTE
-            int b = Native.sys(27, this.bufferAddr + i, 0, 0, 0); 
-            if (b == 0) break; // Fin de la cadena
-            bytes[length++] = (byte) b;
-        }
-        
-        // Evitando el constructor complejo ([CII)V
-        String fullStr = new String(bytes); 
-        return fullStr.substring(0, length);
+        int resultRead = Native.sys(Native.SYS_DISK_READ, lba, 0, bufferAddr, 0);
+        if(resultRead == 1){
+			byte[] bytes = new byte[512];
+			int length = 0;
+			// leer todo el bloque de 512 bytes
+			for (int i = 0; i < 512; i++) {
+				// Syscall 27: SYS_MEM_READ_BYTE
+				int b = Native.sys(27, bufferAddr + i, 0, 0, 0); 
+				if (b == 0) break; // Fin de la cadena
+				bytes[length++] = (byte) b;				
+			}
+			
+			// Evitando el constructor complejo ([CII)V
+			String fullStr = new String(bytes); 
+			return fullStr;
+		}
+		// devolver string vacio, evitar NULL
+		return new String();
     }
-
+	
     // Escribe un arreglo de bytes crudos al disco
     public boolean writeSector(int lba, byte[] data) {
         if (data == null || data.length > 512) return false;
         
-        // Relleno con 0 el buffer
-        for (int i = 0; i < 512; i++) {
+		// Limpiar el bloque inicial de 512 bytes en RAM con 0s
+		for (int i = 0; i < 512; i++) {
 			// Syscall 26: SYS_MEM_WRITE_BYTE
-            Native.sys(26, this.bufferAddr + i, 0, 0, 0); 
+            Native.sys(26, bufferAddr + i, 0, 0, 0);
         }
         
-        // Escribo en la RAM física el arreglo
+        // Escribo en el bloque inicial el arreglo
         for (int i = 0; i < data.length; i++) {
 			// Syscall 26: SYS_MEM_WRITE_BYTE
-            Native.sys(26, this.bufferAddr + i, data[i], 0, 0);
+            Native.sys(26, bufferAddr + i, data[i], 0, 0);
         }
         
-        // Escribo en disco
+        // Escribo en disco y espero resultado: 0-fallo | 1-ok
 		// Syscall 9: SYS_DISK_WRITE
-        int result = Native.sys(Native.SYS_DISK_WRITE, lba, 0, this.bufferAddr, 0);
+        int result = Native.sys(Native.SYS_DISK_WRITE, lba, 0, bufferAddr, 0);
         return (result == 1);
     }
 
 	// Escribe un String directamente en un sector
     public boolean writeString(int lba, String text) {
-        if (text == null) return false;        	
-        
-		byte[] bytes = text.getBytes();
-        
+        if (text == null) return false;        
+		byte[] bytes = text.getBytes();        
         return writeSector(lba, bytes);
     }
 	
