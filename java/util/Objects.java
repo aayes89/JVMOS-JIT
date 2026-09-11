7e90c2ee76a073e416aff6a757fed1cca6acc78d
Allan (Slam) <72238104+aayes89@users.noreply.github.com>
2026-09-11 14:09:55 -0600

    Enhance Objects class with utility methods

--- java/util/Objects.java
+++ java/util/Objects.java
@@ -21,6 +21,83 @@ OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 SOFTWARE.*/
 package java.util;
 
-public class Objects{
+import java.util.Arrays;
+import java.util.Comparator;
 
+public final class Objects {
+  private Objects() {}
+
+  // Retorna 0 si a==b o c.compare(a, b)
+  public static <T> int compare(T a, T b, Comparator<? super T> c) {
+    if (a == b) {
+      return 0;
+    }
+    return c.compare(a, b);
+  }
+
+  // True si ambos argumentos son null
+  public static boolean deepEquals(Object a, Object b) {
+    if (a == null || b == null) {
+      return a == b;
+    } else if (a instanceof Object[] && b instanceof Object[]) {
+      return Arrays.deepEquals((Object[]) a, (Object[]) b);
+    } else if (a instanceof boolean[] && b instanceof boolean[]) {
+      return Arrays.equals((boolean[]) a, (boolean[]) b);
+    } else if (a instanceof byte[] && b instanceof byte[]) {
+      return Arrays.equals((byte[]) a, (byte[]) b);
+    } else if (a instanceof char[] && b instanceof char[]) {
+      return Arrays.equals((char[]) a, (char[]) b);
+    } else if (a instanceof double[] && b instanceof double[]) {
+      return Arrays.equals((double[]) a, (double[]) b);
+    } else if (a instanceof float[] && b instanceof float[]) {
+      return Arrays.equals((float[]) a, (float[]) b);
+    } else if (a instanceof int[] && b instanceof int[]) {
+      return Arrays.equals((int[]) a, (int[]) b);
+    } else if (a instanceof long[] && b instanceof long[]) {
+      return Arrays.equals((long[]) a, (long[]) b);
+    } else if (a instanceof short[] && b instanceof short[]) {
+      return Arrays.equals((short[]) a, (short[]) b);
+    }
+    return a.equals(b);
+  }
+
+  // Son iguales?
+  public static boolean equals(Object a, Object b) {
+    return (a == null) ? (b == null) : a.equals(b);
+  }
+
+  // Calcula el hash del arreglo
+  public static int hash(Object... values) {
+    return Arrays.hashCode(values);
+  }
+
+  // Devuelve 0 o null 
+  public static int hashCode(Object o) {
+    return (o == null) ? 0 : o.hashCode();
+  }
+
+  // Retorna objeto si no es null, sino excepción
+  public static <T> T requireNonNull(T o) {
+    if (o == null) {
+      throw new NullPointerException();
+    }
+    return o;
+  }
+
+  // Retorna objeto si no es null, sino excepción con el mensaje
+  public static <T> T requireNonNull(T o, String message) {
+    if (o == null) {
+      //throw new NullPointerException(message);
+	  System.out.println("Objects: NullPointerException." + message);
+    }
+    return o;
+  }
+
+  // Retorna cadena de texto equivalente del objeto sino null
+  public static String toString(Object o) {
+    return (o == null) ? "null" : o.toString();
+  }
+  public static String toString(Object o, String nullString) {
+    return (o == null) ? nullString : o.toString();
+  }
 }
