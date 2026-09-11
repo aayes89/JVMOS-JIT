13297643111e210856b5006f05858c470d4ccd9b
Allan (Slam) <72238104+aayes89@users.noreply.github.com>
2026-09-11 00:35:38 -0600

    Add new color constants for Windows UI

--- java/awt/Color.java
+++ java/awt/Color.java
@@ -33,6 +33,11 @@ public class Color {
     public static final int LIGHT_GRAY  = 0x00C0C0C0;
     public static final int DARK_GRAY   = 0x00404040;
     public static final int TRANSPARENT = 0x00000000; // Dependiendo de tu alfa
+	public static final int WIN_BG 		= 0x00E0E0E0; 
+	public static final int WIN_TITLE 	= 0x00000080;
+    public static final int SEL 		= 0x00D0D0FF;
+	public static final int FOLDER 		= 0x00F0C000; 
+	public static final int	FILE		= 0x00A0A0A0;
 
     private final int rgb;
 	
