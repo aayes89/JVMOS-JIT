5d8decee2d15970470ba5cae8d01dc799be800a0
Allan (Slam) <72238104+aayes89@users.noreply.github.com>
2026-09-13 12:47:36 -0600

    Add package declaration and import statement

--- java/io/ReadOnlyBufferException.java
+++ java/io/ReadOnlyBufferException.java
@@ -20,6 +20,10 @@ LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
 OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 SOFTWARE.*/
 
+package java.io;
+
+import java.lang.UnsupportedOperationException;
+
 public class ReadOnlyBufferException extends UnsupportedOperationException {
 	// Tomado de implementeación original
     private static final long serialVersionUID = -1210063976496234090L;
