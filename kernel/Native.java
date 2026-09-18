fe493b50f6cd67ebbe6a8f3f23e0f5a7b3d3c9cb
Allan (Slam) <72238104+aayes89@users.noreply.github.com>
2026-09-18 11:39:06 -0600

    Add RTL8168 network card support to Native class

--- kernel/Native.java
+++ kernel/Native.java
@@ -63,7 +63,10 @@ public class Native {
 	public static final int SYS_PCNET_SEND		 = 32; // Enviar paquete con tarjeta de red PCnet
 	public static final int SYS_PCNET_RECEIVE	 = 33; // Recibir paquete con tarjeta de red PCnet
 	public static final int SYS_CONTEXT_SWITCH   = 34; // Ceder control al procesador del siguiente hilo en cola
-	public static final int SYS_MEM_WRITE_DWORD  = 35; // Escribir DWORD en memoria
+	public static final int SYS_MEM_WRITE_DWORD  = 35; // Escribir DWORD en memoria	
+	public static final int SYS_RTL8168_INIT     = 36; // Inicializar Tarjeta Gigabit
+    public static final int SYS_RTL8168_SEND     = 37; // Enviar paquete Gigabit
+    public static final int SYS_RTL8168_RECEIVE  = 38; // Recibir paquete Gigabit
 
 
     // FIRMAS NATIVAS
