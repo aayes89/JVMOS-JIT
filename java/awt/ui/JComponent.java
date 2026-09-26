c8823f87fa6aea3bc7e370f4b2a1f4a969aaec3a
Allan (Slam) <72238104+aayes89@users.noreply.github.com>
2026-09-26 11:07:50 -0600

    Add getters and setters for component properties

--- java/awt/ui/JComponent.java
+++ java/awt/ui/JComponent.java
@@ -61,6 +61,37 @@ public abstract class JComponent {
     }
     
 	// Getters y Setters
-    public void setVisible(boolean v) { this.visible = v; }
-    public boolean isVisible() { return visible; }
+    public void setVisible(boolean v) {
+		this.visible = v; 
+	}
+    public boolean isVisible() { 
+		return visible; 
+	}
+	public int getX(){
+		return x;
+	}
+	public void setX(int x){
+		this.x = x;
+	}
+    public int getY(){
+		return y;
+	}
+	public void setY(int y){
+		this.y = y;
+	}
+    public int getWidth(){
+		return width;
+	}
+	public void setWidth(int width){
+		this.width = width;
+	}
+    public int getHeight(){
+		return height;
+	}
+	public void setHeight(int height){
+		this.height = height;
+	}
+    public JComponent getParent(){
+		return parent;
+	}	
 }
