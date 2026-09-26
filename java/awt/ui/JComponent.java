28c65ac4cc09766d278dd115d759b0446e75aa53
Allan (Slam) <72238104+aayes89@users.noreply.github.com>
2026-09-26 09:20:40 -0600

    Add JComponent class for UI components

---
+++ java/awt/ui/JComponent.java
@@ -0,0 +1,66 @@
+/*
+MIT License
+
+Copyright (c) 2026 Allan (Slam)
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.*/
+
+package java.awt.ui;
+
+import java.awt.Graphics2D;
+
+public abstract class JComponent {
+    protected int x;
+    protected int y;
+    protected int width;
+    protected int height;
+    protected boolean visible;
+    
+    // Jerarquía
+    protected JComponent parent;
+
+	// Constructor
+    public JComponent(int x, int y, int width, int height) {
+        this.x = x;
+        this.y = y;
+        this.width = width;
+        this.height = height;
+        this.visible = true;
+    }
+
+    public abstract void paint(Graphics2D g);
+
+    // Retorna true si el evento fue consumido por este componente
+    public boolean handleMouse(int mx, int my, int btn) {
+        return false; 
+    }
+
+    public boolean handleKey(int key) {
+        return false;
+    }
+
+    public boolean contains(int mx, int my) {
+        // Asume coordenadas absolutas para simplificar en la fase 1
+        return (mx >= x && mx <= x + width && my >= y && my <= y + height);
+    }
+    
+	// Getters y Setters
+    public void setVisible(boolean v) { this.visible = v; }
+    public boolean isVisible() { return visible; }
+}
